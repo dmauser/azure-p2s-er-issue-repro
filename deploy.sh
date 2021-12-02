4f9968ad20a342488dc7ab0d9a5ab3491fca1bec
Daniel Mauser <dmauser@hotmail.com>
2021-12-01 21:31:18 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ echo "*** It will take around 30 minutes to finish the deployment ***"
 az group create --name $rg --location $location
 az deployment group create --name P2SRepro-$RANDOM --resource-group $rg \
 --template-uri https://raw.githubusercontent.com/dmauser/azure-p2s-er-issue-repro/main/azuredeploy.json \
---parameters gatewaySku=VpnGw2 vpnGatewayGeneration=Generation2 Restrict_SSH_VM_AccessByPublicIP=$mypip
+--parameters gatewaySku=VpnGw2 vpnGatewayGeneration=Generation2 Restrict_SSH_VM_AccessByPublicIP=$mypip \
+--output none
 
 # Configure P2S VPN on Azure VPN Gateway
 #Variables
@@ -21,7 +22,8 @@ az network vnet-gateway update -g $rg -n $gwname \
 --client-protocol OpenVPN IkeV2  \
 --address-prefixes 172.16.50.0/24 \
 --root-cert-name LabRootCA \
---root-cert-data cert/labrootca.cer
+--root-cert-data cert/labrootca.cer \
+--output none
 
 #Download VPN profiles 
 az network vnet-gateway vpn-client generate --name $gwname --processor-architecture Amd64 --resource-group $rg 
