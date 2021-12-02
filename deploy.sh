d71642b21eea2adb05d686c501cc3a130aaf7296
Daniel Mauser <dmauser@hotmail.com>
2021-12-01 18:18:00 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #Set parameters (modify for your needs)
 rg=p2s-lab2 #Resource Group Name
 location=eastus #Region
-mypip=$(curl -4 ifconfig.io/ip -s)
+mypip=$(curl -4 ifconfig.io/ip -s) #if you are deploying over Cloudshell set that manually mypip=
 
-#Deploy base
+#Deploy base lab environment = Hub with ER and VPN Gateway + VM and two Spokes with one VM on each.
+echo "***  Note you will be prompted by username and password ***"
+echo "*** It will take around 30 minutes to finish the deployment ***"
 az group create --name $rg --location $location
 az deployment group create --name P2SRepro-$RANDOM --resource-group $rg \
---template-uri https://raw.githubusercontent.com/dmauser/azure-p2s-er-issue-repro/main/azuredeploy.json \
+--template-uri https://raw.githubusercontent.com/dmauser/azure-p2s-er-issue-repro/main/azuredeploy.json?token=AFZBGO6J7BIGI52SOWT6WQTBVAHBG \
 --parameters gatewaySku=VpnGw2 vpnGatewayGeneration=Generation2 Restrict_SSH_VM_AccessByPublicIP=$mypip
 
-# Note you will be prompted by username and password
-
+# Configure P2S VPN on Azure VPN Gateway
 #Variables
 gwname=Az-Hub-vpngw
 vnet=Az-Hub-vnet
-
 
 #Point to Site config OpenVPN + IKEv2 + Certificate
 az network vnet-gateway update -g $rg -n $gwname \
@@ -22,3 +22,10 @@ az network vnet-gateway update -g $rg -n $gwname \
 --address-prefixes 172.16.50.0/24 \
 --root-cert-name LabRootCA \
 --root-cert-data cert/labrootca.cer
+
+#Download VPN profiles 
+az network vnet-gateway vpn-client generate --name $gwname --processor-architecture Amd64 --resource-group $rg 
+vpnurl=$(az network vnet-gateway vpn-client show-url -g $rg -n $gwname -o tsv)
+echo "*** Copy $vpnurl to download client on P2S VPN client ***"
+
+#Install Certificate on P2SVPN Client
